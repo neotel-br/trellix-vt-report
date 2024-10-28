af5bb6ba8c573fc0a4e34932554d7fa8681380f1
Matheus Cantiere <161778601+matheuscantiere@users.noreply.github.com>
2024-10-28 15:23:03 -0300

    Arrumado a função de continuar e cancelar a operação. Adicionado a função de escolher o nome do arquivo que vai ser gerado (#3)
    * Repair Trelix
    
    * Update main.py
    
    * New features and repair
    
    Arrumado a função de continuar e cancelar a operação
    
    Adicionado a função de escolher o nome do arquivo que vai ser gerado

--- main.py
+++ main.py
@@ -34,43 +34,49 @@ def main():
 
     HEADERS = {'x-apikey': args.apikey, 'accept': 'application/json'}
 
+    nome_arquivo = input("Digite o nome do arquivo csv final: ")
+
     inp = input(
-        f"ESSE RELATORIO CONTEM: {len(hashes_unicos)} hashes. Deseja continuar?(Y/N)")
+        f"ESSE RELATORIO CONTEM: {len(hashes_unicos)} hashes. Deseja continuar?(Y/N): ")
 
-    for hash_arq in hashes_unicos:
-        resposta = get(
-            f"{VIRUSTOTAL_FILE_ENDPOINT}/{hash_arq}", headers=HEADERS)
 
-        print(resposta.status_code)
+    if inp.upper() == 'Y':
+        for hash_arq in hashes_unicos:
+            resposta = get(
+                f"{VIRUSTOTAL_FILE_ENDPOINT}/{hash_arq}", headers=HEADERS)
 
-        if resposta.status_code != 200:
-            continue
+            print(resposta.status_code)
 
-        print(hash_arq)
-        resultado_sem_filtro = resposta.json()['data']['attributes']
-        resultado = {}
+            if resposta.status_code != 200:
+                continue
 
-        resultado["Possível Tipo de Ameaça"] = "N/A" if "popular_threat_classification" not in resultado_sem_filtro else resultado_sem_filtro['popular_threat_classification']['suggested_threat_label']
-        resultado["Número de detecções maliciosas"] = resultado_sem_filtro['last_analysis_stats']['malicious']
-        resultado["Número de detecções não-maliciosas"] = resultado_sem_filtro['last_analysis_stats']['harmless']
-        resultado["Número de não detecções"] = resultado_sem_filtro['last_analysis_stats']['undetected']
+            print(hash_arq)
+            resultado_sem_filtro = resposta.json()['data']['attributes']
+            resultado = {}
 
-        dados_sem_duplicados_dict = dados_sem_duplicados.to_dict('index')
+            resultado["Possível Tipo de Ameaça"] = "N/A" if "popular_threat_classification" not in resultado_sem_filtro else resultado_sem_filtro['popular_threat_classification']['suggested_threat_label']
+            resultado["Número de detecções maliciosas"] = resultado_sem_filtro['last_analysis_stats']['malicious']
+            resultado["Número de detecções não-maliciosas"] = resultado_sem_filtro['last_analysis_stats']['harmless']
+            resultado["Número de não detecções"] = resultado_sem_filtro['last_analysis_stats']['undetected']
 
-        for _, v in dados_sem_duplicados_dict.items():
-            v["Possível Tipo de Ameaça"] = resultado["Possível Tipo de Ameaça"]
-            v["Número de detecções maliciosas"] = resultado["Número de detecções maliciosas"]
-            v["Número de detecções não-maliciosas"] = resultado["Número de detecções não-maliciosas"]
-            v["Número de não detecções"] = resultado["Número de não detecções"]
+            dados_sem_duplicados_dict = dados_sem_duplicados.to_dict('index')
 
-    dados = [v for _, v in dados_sem_duplicados_dict.items()]
-    
-    with open('saida2', 'w', newline="") as f:
-            campos = dados[0].keys()
-            escritor_csv = csv.DictWriter(f, fieldnames=campos)
-            
-            escritor_csv.writeheader()
-            escritor_csv.writerows(dados)
+            for _, v in dados_sem_duplicados_dict.items():
+                v["Possível Tipo de Ameaça"] = resultado["Possível Tipo de Ameaça"]
+                v["Número de detecções maliciosas"] = resultado["Número de detecções maliciosas"]
+                v["Número de detecções não-maliciosas"] = resultado["Número de detecções não-maliciosas"]
+                v["Número de não detecções"] = resultado["Número de não detecções"]
+                
+        dados = [v for _, v in dados_sem_duplicados_dict.items()]
+        
+        with open(nome_arquivo, 'w', newline="") as f:
+                campos = dados[0].keys()
+                escritor_csv = csv.DictWriter(f, fieldnames=campos)
+                
+                escritor_csv.writeheader()
+                escritor_csv.writerows(dados)
+    else:
+        print("Relatório cancelado")
 
 if __name__ == "__main__":
     main()
